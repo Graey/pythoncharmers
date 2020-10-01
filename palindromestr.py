30817d7d6e14b8f53215d6a59845e719f20118ec
Pranav <37763901+Graey@users.noreply.github.com>
2020-10-01 16:50:06 +0530

    Program to check if a string is palindrome or not
    value of my_str can be changed to test.

---
+++ palindromestr.py
@@ -0,0 +1,10 @@
+my_str = 'aIbohPhoBiA'
+
+my_str = my_str.casefold()
+
+rev_str = reversed(my_str)
+
+if list(my_str) == list(rev_str):
+   print("The string is a palindrome.")
+else:
+   print("The string is not a palindrome.")

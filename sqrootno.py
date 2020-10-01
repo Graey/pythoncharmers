0b67371326d0e61f0e3edad546de86a385c4165d
Pranav <37763901+Graey@users.noreply.github.com>
2020-10-01 16:48:11 +0530

    Sq. root of real or complex no,

---
+++ sqrootno.py
@@ -0,0 +1,9 @@
+
+import cmath
+
+num = eval(input('Enter a number: '))
+
+
+
+num_sqrt = cmath.sqrt(num)
+print('The square root of {0} is {1:0.3f}+{2:0.3f}j'.format(num ,num_sqrt.real,num_sqrt.imag))

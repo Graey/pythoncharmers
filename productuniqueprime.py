a6fb9f476039ee77ebf6750fa41beb73e5543f45
lettersenchanted <72257709+lettersenchanted@users.noreply.github.com>
2020-10-02 18:06:05 +0530

    Product of unique prime factors of a number
    To est, change the value of n.

---
+++ productuniqueprime.py
@@ -0,0 +1,23 @@
+def productPrimeFactors(n): 
+    product = 1
+      
+    for i in range(2, n+1): 
+        if (n % i == 0): 
+            isPrime = 1
+              
+            for j in range(2, int(i/2 + 1)): 
+                if (i % j == 0): 
+                    isPrime = 0
+                    break
+                  
+            
+            if (isPrime): 
+                product = product * i 
+                  
+    return product   
+      
+      
+      
+# main() 
+n = 44
+print (productPrimeFactors(n)) 

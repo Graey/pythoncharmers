d2de12c3dc192f62b8b408b1fa24991f3a1dffea
Tushark02 <72902440+Tushark02@users.noreply.github.com>
2020-10-15 22:17:23 +0530

    Program of sum or diff or prdct or qotient of 2 no

---
+++ sumordifforproductorquotientof2numbers.py
@@ -0,0 +1,20 @@
+num = 150000
+
+num1 = 100
+
+add = num + num1;
+
+print('Sum of {0} and {1} is {2}' .format(num,num1,add))
+
+prod = num * num1;
+
+print('Product of {0} and {1} is {2}' .format(num,num1,prod))
+
+Diff = num - num1;
+
+print('Difference of {0} and {1} is {2}' .format(num,num1,Diff))
+
+Quot = num/num1;
+
+print('Quotient of {0} and {1} is {2}' .format(num,num1,Quot))
+
